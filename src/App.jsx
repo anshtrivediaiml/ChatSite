615ddb318377ab193f420f0fcf115b82eb830cee
ANSHTRIVEDIAIML <22aiml056@charusat.edu.in>
2024-06-24 23:35:59 +0530

    User signup and login functionality working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import List from './components/list/List'
@@ -6,10 +6,22 @@ import Chat from './components/chat/Chat'
 import Detail from './components/detail/Detail'
 import Login from './components/login/Login'
 import Notification from './components/notification/Notification'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './lib/firebase'
 
 
 function App() {
  const user=false;
+
+ useEffect(()=>{
+  const unSub=onAuthStateChanged(auth,(user)=>{
+    console.log(user)
+  });
+
+  return ()=>{
+    unSub();
+  }
+ },[])
 
   return (
     <>
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import './Login.css'
 import { toast } from 'react-toastify'
-import { auth } from '../../lib/firebase'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth, db } from '../../lib/firebase'
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
+import upload from '../../lib/upload'
 const Login = () => {
     const [avatar,setAvatar] = useState({
         file:null,
         url:""
     })
+    const[loading,setLoading]=useState(false);
 
     const handleAvatar=(e)=>{
         if(e.target.files[0]){
@@ -22,22 +25,61 @@ const Login = () => {
 
     const handleRegister= async(e)=>{
         e.preventDefault();
+        setLoading(true);
         const formData = new FormData(e.target);
         const { username,email,password}=Object.fromEntries(formData);
   
         try{
             
           const res = await createUserWithEmailAndPassword(auth,email,password); 
+          //Uploading the user details to the firebase firestore database and blocked is an array which will contain the user ids of the users who are blocked by the user
+
+          const imgUrl= await upload(avatar.file);
+          //Uploading the avatar to the firebase storage
+          await setDoc(doc(db,"users",res.user.uid),{
+           username,
+            email,
+            avatar:imgUrl,
+            id:res.user.uid,
+            blocked:[],
+          
+          });
+           
+          //Also uploading the chats of users with other users to the firestore db
+          await setDoc(doc(db,"userchats",res.user.uid),{
+            chats:[]
+          })
+
+          toast.success("Account Created Successfully!You can login now")
          
         }
         catch(err){
           console.log(err)
           toast.error(err.message)
         }
+        finally{
+            setLoading(false);
+        }
   
     }
 
-    const handleLogin=(e)=>{}
+    const handleLogin=async(e)=>{
+      e.preventDefault();
+      setLoading(true);
+      const Formdata = new FormData(e.target);
+      const {email,password}=Object.fromEntries(Formdata);
+
+      try{
+        const res= await signInWithEmailAndPassword(auth,email,password);
+        toast.success("Logged in Successfully");
+        
+      }catch(err){
+        toast.error(err.message)
+      }
+      finally{
+        setLoading(false);
+      }
+    }
   return (
     <div className='login'>
       <div className='item'>
@@ -45,13 +87,13 @@ const Login = () => {
         <form onSubmit={handleLogin}>
             <input type="email" name="email"   placeholder='Email'/>
             <input type="password" name="password" placeholder='Password'/>
-            <button>Sign In</button>
+            <button disabled={loading}>{loading?"Loading":"Sign In"}</button>
         </form>
       </div>
       <div className='separator'></div>
       <div className='item'>
       <h2>Create an Account</h2>
-        <form onClick={handleRegister}>
+        <form onSubmit={handleRegister}>
             <label htmlFor='file'>
                 <img src={avatar.url || "./avatar.png"} alt="userimg"/>
                 Upload an Image</label>
@@ -59,7 +101,7 @@ const Login = () => {
             <input type="text" name="username"  placeholder='Username'/>
             <input type="text" name="email"  placeholder='Email'/>
             <input type="password" name="password"  placeholder='Password'/>
-            <button>Sign Up</button>
+            <button disabled={loading}>{loading?"Loading":"Sign Up"}</button>
         </form>
       </div>
     </div>
